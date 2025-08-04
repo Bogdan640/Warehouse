7f49f7dbddf0ef98c6b01ab0df46d0e0a5c9a989
Bogdan640 <bogdanbubu2004@gmail.com>
2025-08-04 10:40:26 +0300

    finish product class

--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Product {
 
@@ -44,6 +45,32 @@ public class Product {
         return 1.0; // Took as default for crackers pack
 
     }
+
+    private void manageExtraFields() {
+
+        switch (productCategory) {
+            case FRUITS:
+                extraFields.put("nutritional quality",  generateRandomNutritionalQuality());
+                break;
+            case VEGETABLES:
+                extraFields.put("nutritional quality",  generateRandomNutritionalQuality());
+                extraFields.put("producer", generateRandomProducer());
+                break;
+            case OTHERS:
+                break;
+        }
+    }
+
+    private String generateRandomNutritionalQuality() {
+        String qualities[] = {"1st quality", "2nd quality", "3rd quality"};
+        return qualities[new Random().nextInt(qualities.length)];
+    }
+
+    private String generateRandomProducer() {
+        String producer[] = {"Spain", "Italy", "Romania", "Greece", "Turkey"};
+        return producer[new Random().nextInt(producer.length)];
+    }
+
 
     public String getName() {
         return name;
